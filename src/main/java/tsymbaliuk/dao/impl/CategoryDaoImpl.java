dfaa985ccde24e17af035ac3e8b5007d089fbf9e
Цымбалюк Сергей <sergdefault@gmail.com>
2016-08-31 13:03:56 +0300

    hibernate entity

--- src/main/java/tsymbaliuk/dao/impl/CategoryDaoImpl.java
+++ src/main/java/tsymbaliuk/dao/impl/CategoryDaoImpl.java
@@ -30,25 +30,18 @@ public class CategoryDaoImpl implements CategoryDAO {
 
     @Override
     public void updateCategory(int category_id, Category category) throws SQLException {
-        try {
             session.beginTransaction();
             Category cat= session.load(Category.class,category_id);
             cat.setDescription(category.getDescription());
             cat.setTitle(category.getTitle());
             session.flush();
             session.getTransaction().commit();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (session != null && session.isOpen()) {
-                session.close();
-            }
-        }
     }
 
     @Override
-    public Category getCategoryById(Long category_id) throws SQLException {
-        return null;
+    public Category getCategoryById(int category_id) throws SQLException {
+        Category cat= session.load(Category.class,category_id);
+        return cat;
     }
 
     @Override
@@ -69,23 +62,9 @@ public class CategoryDaoImpl implements CategoryDAO {
 
     @Override
     public void deleteCategory(Category category) throws SQLException {
-
-    }
-
-    @Override
-    public Collection getCategoriesByProduct(Product product) throws SQLException {
-        return null;
-    }
-
-    public static void main(String[] args) {
-        Category cat = new Category();
-        cat.setTitle("43214321");
-        cat.setDescription("43214321");
-        try {
-            new CategoryDaoImpl().updateCategory(2,cat);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        session.beginTransaction();
+        session.delete(category);
+        session.getTransaction().commit();
     }
 }
 
